3cbe66570d724a4c5330d11998eb0642bc343c23
Roman-Glyshanitsya <r.glyshanitsya@gmail.com>
2024-12-29 02:11:18 +0200

    loader started

---
+++ src/js/loader.js
@@ -0,0 +1,6 @@
+function hideLoader() {
+  const loader = document.querySelector('.loader');
+  loader.style.display = 'none';
+}
+
+hideLoader();
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import './js/loader';
 import './js/parallax';
 import './js/progress-bar';
 import './js/sound';
